a8e4064c5abdb4883fd0da091d95747ae764c76c
Jamie Davidson <roastmastersp@gmail.com>
2021-11-15 12:03:51 +0000

    Add structures for cells and cell collections

---
+++ HanjieSharp/Domain/Cell.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HanjieSharp.Domain
+{
+    internal sealed class Cell
+    {
+        public int X { get; }
+        public int Y { get; }
+        public CellState State { get; private set; }
+
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+            State = CellState.None;
+        }
+
+        public void UpdateNodeState(CellState newState)
+        {
+            State = newState;
+        }
+
+        public override string ToString()
+        {
+            return State switch
+            {
+                CellState.None => " ",
+                CellState.Yes => "O",
+                CellState.No => "X",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+    }
+
+    internal enum CellState
+    {
+        None,
+        Yes,
+        No
+    }
+}
---
+++ HanjieSharp/Domain/CellCollection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HanjieSharp.Domain
+{
+    internal sealed class CellCollection
+    {
+        public int Index { get; }
+        public IReadOnlyCollection<int> Hints { get; }
+        public IReadOnlyCollection<Cell> Cells { get; }
+
+        public CellCollection(int index, IReadOnlyCollection<int> hints, IReadOnlyCollection<Cell> cells)
+        {
+            Index = index;
+            Hints = hints;
+            Cells = cells;
+        }
+
+        public bool IsComplete()
+        {
+            return Cells.All(c => c.State != CellState.None);
+        }
+
+        public override string ToString()
+        {
+            return string.Join("", Cells.Select(c => c.ToString()));
+        }
+    }
+}
