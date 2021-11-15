a79cb21aafa793d0ddd88793978bbb8cfd9d0781
Jamie Davidson <roastmastersp@gmail.com>
2021-11-15 12:05:18 +0000

    Stitch solvers together with grid solution

---
+++ HanjieSharp/Domain/Grid.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HanjieSharp.Solvers;
+
+namespace HanjieSharp.Domain
+{
+    internal sealed class Grid
+    {
+        private readonly IRowSolver[] m_RuleSolvers =
+        {
+            new TrivialRowSolver(),
+            new GuaranteedCellSolver(),
+            new FillInTheBlanksSolver(),
+        };
+        
+        public int ColumnCount { get; }
+        public int RowCount { get; }
+        public IReadOnlyCollection<CellCollection> Rows { get; }
+        public IReadOnlyCollection<CellCollection> Columns { get; }
+
+        public Grid(int columnCount, int rowCount, IEnumerable<int[]> columnHints, IEnumerable<int[]> rowHints)
+        {
+            ColumnCount = columnCount;
+            RowCount = rowCount;
+
+            var allCells = new List<Cell>();
+
+            for (var x = 0; x < columnCount; x++)
+            {
+                for (var y = 0; y < rowCount; y++)
+                {
+                    allCells.Add(new Cell(x, y));
+                }
+            }
+
+            Columns = columnHints
+                .Select((h, i) => new CellCollection(i, h, allCells.Where(c => c.X == i).ToArray()))
+                .ToArray();
+            
+            Rows = rowHints
+                .Select((h, i) => new CellCollection(i, h, allCells.Where(c => c.Y == i).ToArray()))
+                .ToArray();
+        }
+
+        public void Solve()
+        {
+            foreach (var solver in m_RuleSolvers)
+            {
+                var allCollections = Columns.Concat(Rows);
+
+                foreach (var collection in allCollections)
+                {
+                    if (collection.IsComplete())
+                    {
+                        continue;
+                    }
+                    
+                    solver.Solve(collection);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, Rows.Select(r => r.ToString()));
+        }
+    }
+}
---
+++ HanjieSharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using HanjieSharp.Domain;
+
+namespace HanjieSharp
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var columnHints = new[]
+            {
+                new[] {10},
+                new[] {1, 1},
+                new[] {1, 1, 1, 1, 1},
+                new[] {1, 1, 1, 1, 1},
+                new[] {1, 1, 1, 1, 1},
+                new[] {1, 1},
+                new[] {1, 1},
+                new[] {1, 1, 1},
+                new[] {1, 1},
+                new[] {10}
+            };
+
+            var rowHints = new[]
+            {
+                new[] {10},
+                new[] {1, 1},
+                new[] {1, 1, 1},
+                new[] {1, 3, 1},
+                new[] {1, 1},
+                new[] {1, 3, 1},
+                new[] {1, 1},
+                new[] {1, 3, 1},
+                new[] {1, 1},
+                new[] {10}
+            };
+            
+            var grid = new Grid(10, 10, columnHints, rowHints);
+            
+            grid.Solve();
+            
+            Console.WriteLine(grid.ToString());
+        }
+    }
+}
