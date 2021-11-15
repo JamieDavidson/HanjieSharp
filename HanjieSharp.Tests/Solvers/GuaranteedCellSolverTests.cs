cb2e81175a8fab9fd59a92b46c2422fc3e8c5304
Jamie Davidson <roastmastersp@gmail.com>
2021-11-15 12:04:29 +0000

    Add guaranteed cell solver

---
+++ HanjieSharp.Tests/Solvers/GuaranteedCellSolverTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HanjieSharp.Domain;
+using HanjieSharp.Solvers;
+using NUnit.Framework;
+
+namespace HanjieSharp.Tests.Solvers
+{
+    public class GuaranteedCellSolverTests
+    {
+        [TestCase("  O  ", 5, 3)]
+        [TestCase(" OOOOOOOO ", 10, 9)]
+        public void Guaranteed_cells_are_calculated_correctly(string expectedAnswer, int length, params int[] hints)
+        {
+            var solver = new GuaranteedCellSolver();
+
+            var cellCollection = new CellCollection(0, hints, CreateCellCollection(length));
+            
+            solver.Solve(cellCollection);
+
+            Assert.That(cellCollection.ToString(), Is.EqualTo(expectedAnswer));
+        }
+
+        [TestCase(5, 1)]
+        [TestCase(5, 2)]
+        [TestCase(7, 3)]
+        [TestCase(10, 5)]
+        public void Cell_collection_with_no_guaranteed_cells_is_left_alone(int length, params int[] hints)
+        {
+            var solver = new GuaranteedCellSolver();
+
+            var cellCollection = new CellCollection(0, hints, CreateCellCollection(length));
+            
+            solver.Solve(cellCollection);
+
+            foreach (var cell in cellCollection.Cells)
+            {
+                Assert.That(cell.State, Is.EqualTo(CellState.None));
+            }
+        }
+        
+        private static IReadOnlyCollection<Cell> CreateCellCollection(int length)
+        {
+            return Enumerable.Range(0, length).Select(i => new Cell(0, i)).ToArray();
+        }
+    }
+}
---
+++ HanjieSharp/Solvers/GuaranteedCellSolver.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using HanjieSharp.Domain;
+
+namespace HanjieSharp.Solvers
+{
+    internal sealed class GuaranteedCellSolver : IRowSolver
+    {
+        public void Solve(CellCollection cellCollection)
+        {
+            if (cellCollection.Hints.Count != 1)
+            {
+                return;
+            }
+
+            var cellCount = cellCollection.Cells.Count;
+            var hint = cellCollection.Hints.First();
+            var difference = cellCount - hint;
+            if (difference >= hint)
+            {
+                return;
+            }
+
+            var guaranteed = cellCollection.Cells.Skip(difference).Take(hint - difference).ToList();
+            guaranteed.ForEach(c => c.UpdateNodeState(CellState.Yes));
+        }
+    }
+}
