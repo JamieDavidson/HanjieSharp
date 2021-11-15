1540d8c89bccbc9e81968a4a8ef08ca4095ea45a
Jamie Davidson <roastmastersp@gmail.com>
2021-11-15 12:05:07 +0000

    Add FillInTheBlanksSolver
    this is badly named :D

---
+++ HanjieSharp/Solvers/FillInTheBlanksSolver.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using HanjieSharp.Domain;
+
+namespace HanjieSharp.Solvers
+{
+    internal sealed class FillInTheBlanksSolver : IRowSolver
+    {
+        public void Solve(CellCollection cellCollection)
+        {
+            if (cellCollection.Cells.Count(c => c.State == CellState.Yes) != cellCollection.Hints.Sum())
+            {
+                return;
+            }
+
+            foreach (var cell in cellCollection.Cells.Where(c => c.State == CellState.None))
+            {
+                cell.UpdateNodeState(CellState.No);
+            }
+        }
+    }
+}
